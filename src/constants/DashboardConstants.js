c4da5a436e1218e5d7625045b6f6b46a07eb72f5
Isaac Gifford <isaac.gifford@octanner.com>
2016-01-08 16:22:53 -0700

    fix invalid syntax for babel

--- src/constants/DashboardConstants.js
+++ src/constants/DashboardConstants.js
@@ -1,8 +1,5 @@
-var BASE_URL = 'http://localhost:3001/';
-export default {
-  BASE_URL: BASE_URL,
-  QUOTE_URL: BASE_URL + 'api/protected/random-quote',
-  QUOTE_GET: 'QUOTE_GET',
-  GROUP_SAVED: 'GROUP_SAVED',
-  GROUPS_GET: 'GROUPS_GET'
-}
+export const BASE_URL = 'http://localhost:3001/';
+export const QUOTE_URL = BASE_URL + 'api/protected/random-quote';
+export const QUOTE_GET = 'QUOTE_GET';
+export const GROUP_SAVED = 'GROUP_SAVED';
+export const GROUPS_GET = 'GROUPS_GET';
--- src/constants/LoginConstants.js
+++ src/constants/LoginConstants.js
@@ -1,8 +1,5 @@
-var BASE_URL = 'http://localhost:3001/';
-export default {
-  BASE_URL: BASE_URL,
-  LOGIN_URL: BASE_URL + 'sessions/create',
-  SIGNUP_URL: BASE_URL + 'users',
-  LOGIN_USER: 'LOGIN_USER',
-  LOGOUT_USER: 'LOGOUT_USER'
-}
+export const BASE_URL = 'http://localhost:3001/';
+export const LOGIN_URL = BASE_URL + 'sessions/create';
+export const SIGNUP_URL = BASE_URL + 'users';
+export const LOGIN_USER = 'LOGIN_USER';
+export const LOGOUT_USER = 'LOGOUT_USER';
